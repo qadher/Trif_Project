a3e83bcfd627304b8d50bd023edeaefa03faef6f
fasil2401 <fasil2401@gmail.com>
2022-03-31 14:06:11 +0530

    Tour inner screen edited

---
+++ lib/screens/tours/components/fixed_top_navigatio.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FixedTopNavigation extends StatelessWidget {
+  const FixedTopNavigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20.h),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 25.w,vertical: 15.h),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.of(context).pop();
+                },
+                child: Icon(Icons.arrow_back_ios,
+                color: Colors.white,
+                size: 25.h,
+                ),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.white.withOpacity(0.6),
+                radius: 20.h,
+                child: Icon(Icons.search,
+                color: Colors.black,),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
